cb91f4b07df9e8c1a0ef8cddc842c0f743e2677d
aminbakkouh <amin.bakkouh.7e7@itb.cat>
2024-02-16 09:49:39 +0100

    first Commit

--- src/main/kotlin/Models/CompteBancari.kt
+++ src/main/kotlin/Models/CompteBancari.kt
@@ -3,9 +3,25 @@ package Models
 import java.sql.Date
 
 abstract class CompteBancari {
-    private var OberturaData:Int=0/0/0
+    private var OberturaData:String="0/0/0"
     private var NomCompte:String=""
     private var Saldo:Double=0.0
+
+    constructor(OberturaData:String, NomCompte:String, Saldo:Double){
+        this.OberturaData=OberturaData
+        this.NomCompte=NomCompte
+        this.Saldo=Saldo
+    }
+
+    abstract fun Ingresar(Saldo: Double):Double{
+        println("Haz el Ingreso")
+        var pSaldo=Saldo
+       var ingreso= readln().toInt()
+        return ingreso+pSaldo
+
+    }
+
+
 
 }
 
--- src/main/kotlin/Models/CompteCorrent.kt
+++ src/main/kotlin/Models/CompteCorrent.kt
@@ -3,12 +3,18 @@ package Models
 class CompteCorrent {
     private var ComisioManteniment:Double=0.0
 
-}
+    constructor(ComisioManteniment:Double){
+        this.ComisioManteniment=ComisioManteniment
+    }
+
+
 
 fun retirar(){
 
 }
 
 fun CobrarComisió(){
+
+}
 
 }
